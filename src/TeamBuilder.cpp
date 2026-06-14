d69be12612536cb3bfbd5b0246f12ce193a38eca
ThaCharly <carlos.gr062008@outlook.com>
2026-06-14 04:52:47 -0300

    JUGADOOOOORES

---
+++ src/TeamBuilder.cpp
@@ -0,0 +1,193 @@
+#include "mfm/TeamBuilder.hpp"
+
+namespace mfm {
+
+Team TeamBuilder::createNacional() {
+    Team t;
+    t.name = "Nacional";
+    t.formation = FormationId::F_433;
+    t.tactics = {
+        .press_style = PressStyle::HIGH_PRESS,
+        .build_up = BuildUpStyle::LONG_BALL,
+        .atk_transition = TransitionStyle::FAST_BREAK,
+        .def_transition = TransitionStyle::IMMEDIATE_PRESS,
+        .defensive_line = 0.8f,
+        .width = 0.4f
+    };
+    
+    t.squad = {
+        {"Luis Mejía [GK]", RoleId::GOALKEEPER, PlayerAttributes{
+            .passing=45, .first_touch=50, .dribbling=10, .shooting=10, .heading=60, 
+            .crossing=10, .vision=45, .positioning=75, .anticipation=70, .composure=75, 
+            .pace=50, .stamina=60, .strength=75, .tackling=10, .marking=10, 
+            .reflexes_gk=85, .positioning_gk=82, .handling_gk=78, .aerial_gk=80
+        }, 0},
+        
+        {"Emiliano Ancheta [DF_R]", RoleId::OFFENSIVE_FULLBACK, PlayerAttributes{
+            .passing=65, .first_touch=68, .dribbling=70, .shooting=50, .heading=65, 
+            .crossing=72, .vision=60, .positioning=70, .anticipation=70, .composure=68, 
+            .pace=80, .stamina=85, .strength=72, .tackling=74, .marking=68, 
+            .reflexes_gk=10, .positioning_gk=10, .handling_gk=10, .aerial_gk=10
+        }, 1},
+        
+        {"Sebastián Coates [DF_C]", RoleId::CENTER_BACK, PlayerAttributes{
+            .passing=68, .first_touch=65, .dribbling=50, .shooting=45, .heading=82, 
+            .crossing=50, .vision=65, .positioning=80, .anticipation=82, .composure=85, 
+            .pace=55, .stamina=70, .strength=88, .tackling=85, .marking=80, 
+            .reflexes_gk=10, .positioning_gk=10, .handling_gk=10, .aerial_gk=10
+        }, 1},
+        
+        {"Agustín Rogel [DF_C]", RoleId::CENTER_BACK, PlayerAttributes{
+            .passing=60, .first_touch=62, .dribbling=45, .shooting=40, .heading=78, 
+            .crossing=45, .vision=55, .positioning=75, .anticipation=78, .composure=70, 
+            .pace=70, .stamina=80, .strength=80, .tackling=80, .marking=75, 
+            .reflexes_gk=10, .positioning_gk=10, .handling_gk=10, .aerial_gk=10
+        }, 1},
+        
+        {"Camilo Cándido [DF_L]", RoleId::DEFENSIVE_FULLBACK, PlayerAttributes{
+            .passing=68, .first_touch=70, .dribbling=65, .shooting=55, .heading=60, 
+            .crossing=75, .vision=65, .positioning=72, .anticipation=74, .composure=70, 
+            .pace=75, .stamina=82, .strength=74, .tackling=76, .marking=72, 
+            .reflexes_gk=10, .positioning_gk=10, .handling_gk=10, .aerial_gk=10
+        }, 1},
+        
+        {"Christian Dos Santos [MC_D]", RoleId::DEFENSIVE_MIDFIELDER, PlayerAttributes{
+            .passing=70, .first_touch=72, .dribbling=60, .shooting=55, .heading=65, 
+            .crossing=55, .vision=65, .positioning=78, .anticipation=80, .composure=75, 
+            .pace=75, .stamina=88, .strength=78, .tackling=82, .marking=78, 
+            .reflexes_gk=10, .positioning_gk=10, .handling_gk=10, .aerial_gk=10
+        }, 2},
+        
+        {"Lucas Boggio [MC_C]", RoleId::BOX_TO_BOX, PlayerAttributes{
+            .passing=75, .first_touch=75, .dribbling=68, .shooting=60, .heading=60, 
+            .crossing=65, .vision=72, .positioning=76, .anticipation=75, .composure=78, 
+            .pace=72, .stamina=85, .strength=76, .tackling=78, .marking=70, 
+            .reflexes_gk=10, .positioning_gk=10, .handling_gk=10, .aerial_gk=10
+        }, 2},
+        
+        {"Diente López [MC_A]", RoleId::ADVANCED_PLAYMAKER, PlayerAttributes{
+            .passing=82, .first_touch=82, .dribbling=80, .shooting=75, .heading=55, 
+            .crossing=75, .vision=85, .positioning=78, .anticipation=65, .composure=80, 
+            .pace=68, .stamina=70, .strength=65, .tackling=50, .marking=45, 
+            .reflexes_gk=10, .positioning_gk=10, .handling_gk=10, .aerial_gk=10
+        }, 2},
+        
+        {"Tomás Verón Lupi [EX_R]", RoleId::WINGER, PlayerAttributes{
+            .passing=68, .first_touch=72, .dribbling=82, .shooting=68, .heading=55, 
+            .crossing=78, .vision=65, .positioning=70, .anticipation=60, .composure=68, 
+            .pace=88, .stamina=84, .strength=65, .tackling=45, .marking=40, 
+            .reflexes_gk=10, .positioning_gk=10, .handling_gk=10, .aerial_gk=10
+        }, 3},
+        
+        {"Maxi Gómez [DC]", RoleId::TARGET_FORWARD, PlayerAttributes{
+            .passing=62, .first_touch=75, .dribbling=68, .shooting=94, .heading=85, 
+            .crossing=50, .vision=65, .positioning=82, .anticipation=70, .composure=80, 
+            .pace=85, .stamina=78, .strength=86, .tackling=40, .marking=35, 
+            .reflexes_gk=10, .positioning_gk=10, .handling_gk=10, .aerial_gk=10
+        }, 3},
+        
+        {"Federico Martínez [EX_L]", RoleId::INSIDE_FORWARD, PlayerAttributes{
+            .passing=72, .first_touch=76, .dribbling=80, .shooting=80, .heading=80, 
+            .crossing=65, .vision=70, .positioning=78, .anticipation=65, .composure=78, 
+            .pace=78, .stamina=75, .strength=82, .tackling=45, .marking=40, 
+            .reflexes_gk=10, .positioning_gk=10, .handling_gk=10, .aerial_gk=10
+        }, 3}
+    };
+    return t;
+}
+
+Team TeamBuilder::createPenarol() {
+    Team t;
+    t.name = "Peñarol";
+    t.formation = FormationId::F_442;
+    t.tactics = {
+        .press_style = PressStyle::MID_BLOCK,
+        .build_up = BuildUpStyle::PATIENT,
+        .atk_transition = TransitionStyle::CONTROLLED,
+        .def_transition = TransitionStyle::REGROUP,
+        .defensive_line = 0.4f,
+        .width = 0.5f
+    };
+    
+    t.squad = {
+        {"Washington Aguerre [GK]", RoleId::GOALKEEPER, PlayerAttributes{
+            .passing=50, .first_touch=55, .dribbling=10, .shooting=10, .heading=60, 
+            .crossing=10, .vision=40, .positioning=72, .anticipation=68, .composure=70, 
+            .pace=55, .stamina=60, .strength=70, .tackling=10, .marking=10, 
+            .reflexes_gk=84, .positioning_gk=80, .handling_gk=75, .aerial_gk=78
+        }, 0},
+        
+        {"Mateo Barboza [DF_R]", RoleId::OFFENSIVE_FULLBACK, PlayerAttributes{
+            .passing=70, .first_touch=72, .dribbling=75, .shooting=60, .heading=55, 
+            .crossing=75, .vision=68, .positioning=70, .anticipation=68, .composure=72, 
+            .pace=82, .stamina=84, .strength=65, .tackling=70, .marking=65, 
+            .reflexes_gk=10, .positioning_gk=10, .handling_gk=10, .aerial_gk=10
+        }, 1},
+        
+        {"Emanuel Gularte [DF_C]", RoleId::CENTER_BACK, PlayerAttributes{
+            .passing=62, .first_touch=65, .dribbling=48, .shooting=45, .heading=85, 
+            .crossing=45, .vision=55, .positioning=78, .anticipation=80, .composure=75, 
+            .pace=60, .stamina=75, .strength=86, .tackling=82, .marking=78, 
+            .reflexes_gk=10, .positioning_gk=10, .handling_gk=10, .aerial_gk=10
+        }, 1},
+        
+        {"Mauricio Lemos [DF_C]", RoleId::CENTER_BACK, PlayerAttributes{
+            .passing=65, .first_touch=68, .dribbling=50, .shooting=40, .heading=80, 
+            .crossing=50, .vision=60, .positioning=76, .anticipation=78, .composure=74, 
+            .pace=65, .stamina=78, .strength=82, .tackling=80, .marking=75, 
+            .reflexes_gk=10, .positioning_gk=10, .handling_gk=10, .aerial_gk=10
+        }, 1},
+        
+        {"Maximiliano Olivera [DF_L]", RoleId::DEFENSIVE_FULLBACK, PlayerAttributes{
+            .passing=65, .first_touch=68, .dribbling=60, .shooting=45, .heading=65, 
+            .crossing=68, .vision=60, .positioning=74, .anticipation=72, .composure=70, 
+            .pace=78, .stamina=80, .strength=75, .tackling=78, .marking=72, 
+            .reflexes_gk=10, .positioning_gk=10, .handling_gk=10, .aerial_gk=10
+        }, 1},
+        
+        {"Jesús Trindade [MC_D]", RoleId::DEFENSIVE_MIDFIELDER, PlayerAttributes{
+            .passing=72, .first_touch=70, .dribbling=62, .shooting=50, .heading=65, 
+            .crossing=55, .vision=68, .positioning=78, .anticipation=82, .composure=76, 
+            .pace=70, .stamina=85, .strength=80, .tackling=84, .marking=78, 
+            .reflexes_gk=10, .positioning_gk=10, .handling_gk=10, .aerial_gk=10
+        }, 2},
+        
+        {"Rodrigo Fernández [MC_C]", RoleId::BOX_TO_BOX, PlayerAttributes{
+            .passing=74, .first_touch=75, .dribbling=70, .shooting=65, .heading=60, 
+            .crossing=68, .vision=75, .positioning=78, .anticipation=74, .composure=75, 
+            .pace=78, .stamina=86, .strength=72, .tackling=76, .marking=68, 
+            .reflexes_gk=10, .positioning_gk=10, .handling_gk=10, .aerial_gk=10
+        }, 2},
+        
+        {"Eduardo Darias [MC_A]", RoleId::ADVANCED_PLAYMAKER, PlayerAttributes{
+            .passing=80, .first_touch=80, .dribbling=78, .shooting=72, .heading=55, 
+            .crossing=75, .vision=82, .positioning=75, .anticipation=65, .composure=78, 
+            .pace=72, .stamina=72, .strength=65, .tackling=55, .marking=50, 
+            .reflexes_gk=10, .positioning_gk=10, .handling_gk=10, .aerial_gk=10
+        }, 2},
+        
+        {"Nahuel Umpierrez [EX_L]", RoleId::WINGER, PlayerAttributes{
+            .passing=72, .first_touch=76, .dribbling=80, .shooting=68, .heading=55, 
+            .crossing=76, .vision=70, .positioning=72, .anticipation=60, .composure=70, 
+            .pace=85, .stamina=80, .strength=65, .tackling=45, .marking=40, 
+            .reflexes_gk=10, .positioning_gk=10, .handling_gk=10, .aerial_gk=10
+        }, 3},
+        
+        {"Facundo Batista [DC]", RoleId::PRESSING_FORWARD, PlayerAttributes{
+            .passing=65, .first_touch=72, .dribbling=70, .shooting=80, .heading=78, 
+            .crossing=55, .vision=65, .positioning=80, .anticipation=75, .composure=78, 
+            .pace=78, .stamina=82, .strength=78, .tackling=50, .marking=45, 
+            .reflexes_gk=10, .positioning_gk=10, .handling_gk=10, .aerial_gk=10
+        }, 3},
+        
+        {"Matías Arezo [EX_R]", RoleId::INSIDE_FORWARD, PlayerAttributes{
+            .passing=70, .first_touch=75, .dribbling=82, .shooting=75, .heading=65, 
+            .crossing=72, .vision=68, .positioning=75, .anticipation=65, .composure=74, 
+            .pace=86, .stamina=78, .strength=70, .tackling=45, .marking=40, 
+            .reflexes_gk=10, .positioning_gk=10, .handling_gk=10, .aerial_gk=10
+        }, 3}
+    };
+    return t;
+}
+
+} // namespace mfm
--- src/main.cpp
+++ src/main.cpp
@@ -1,83 +1,27 @@
-#include "../include/mfm/Team.hpp"
-#include "../include/mfm/Config.hpp"
-#include "../include/mfm/SimulationLoop.hpp"
-#include "../include/mfm/Logger.hpp"
+#include "mfm/Config.hpp"
+#include "mfm/SimulationLoop.hpp"
+#include "mfm/Logger.hpp"
+#include "mfm/TeamBuilder.hpp"
 #include <iostream>
 #include <filesystem>
 #include <chrono>
-
-mfm::Team armarNacional() {
-    mfm::Team t;
-    t.name = "Nacional";
-    t.formation = mfm::FormationId::F_433;
-    t.tactics = {
-        .press_style = mfm::PressStyle::HIGH_PRESS,
-        .build_up = mfm::BuildUpStyle::LONG_BALL,
-        .atk_transition = mfm::TransitionStyle::FAST_BREAK,
-        .def_transition = mfm::TransitionStyle::IMMEDIATE_PRESS,
-        .defensive_line = 0.8f,
-        .width = 0.4f
-    };
-    t.squad = {
-        {"Mejía [GK]",      mfm::RoleId::GOALKEEPER,           {45, 50, 10, 10, 60, 10, 45, 75, 70, 75, 50, 60, 75, 10, 10, 85, 82, 78, 80}, 0},
-        {"Ancheta [DF_R]",  mfm::RoleId::OFFENSIVE_FULLBACK, {65, 68, 70, 50, 65, 72, 60, 70, 70, 68, 80, 85, 72, 74, 68, 10, 10, 10, 10}, 1},
-        {"Coates [DF_C]",   mfm::RoleId::CENTER_BACK,          {68, 65, 50, 45, 82, 50, 65, 80, 82, 85, 55, 70, 88, 85, 80, 10, 10, 10, 10}, 1},
-        {"Rogel [DF_C]",    mfm::RoleId::CENTER_BACK,          {60, 62, 45, 40, 78, 45, 55, 75, 78, 70, 70, 80, 80, 80, 75, 10, 10, 10, 10}, 1},
-        {"Cándido [DF_L]",  mfm::RoleId::DEFENSIVE_FULLBACK, {68, 70, 65, 55, 60, 75, 65, 72, 74, 70, 75, 82, 74, 76, 72, 10, 10, 10, 10}, 1},
-        {"Dos Santos[MC_D]",mfm::RoleId::DEFENSIVE_MIDFIELDER, {70, 72, 60, 55, 65, 55, 65, 78, 80, 75, 75, 88, 78, 82, 78, 10, 10, 10, 10}, 2},
-        {"Boggio [MC_C]",   mfm::RoleId::BOX_TO_BOX,           {75, 75, 68, 60, 60, 65, 72, 76, 75, 78, 72, 85, 76, 78, 70, 10, 10, 10, 10}, 2},
-        {"Diente [MC_A]",   mfm::RoleId::ADVANCED_PLAYMAKER,   {82, 82, 80, 75, 55, 75, 85, 78, 65, 80, 68, 70, 65, 50, 45, 10, 10, 10, 10}, 2},
-        {"Lupi [EX_R]",     mfm::RoleId::WINGER,               {68, 72, 82, 68, 55, 78, 65, 70, 60, 68, 88, 84, 65, 45, 40, 10, 10, 10, 10}, 3},
-        {"Maxi Gomez[DC]",  mfm::RoleId::TARGET_FORWARD,       {62, 75, 68, 94, 85, 50, 65, 82, 70, 80, 85, 78, 86, 40, 35, 10, 10, 10, 10}, 3},
-        {"Martínez [EX_L]", mfm::RoleId::INSIDE_FORWARD,       {72, 76, 80, 80, 80, 65, 70, 78, 65, 78, 78, 75, 82, 45, 40, 10, 10, 10, 10}, 3}
-    };
-    return t;
-}
-
-mfm::Team armarPenarol() {
-    mfm::Team t;
-    t.name = "Peñarol";
-    t.formation = mfm::FormationId::F_442;
-    t.tactics = {
-        .press_style = mfm::PressStyle::MID_BLOCK,
-        .build_up = mfm::BuildUpStyle::PATIENT,
-        .atk_transition = mfm::TransitionStyle::CONTROLLED,
-        .def_transition = mfm::TransitionStyle::REGROUP,
-        .defensive_line = 0.4f,
-        .width = 0.5f
-    };
-    t.squad = {
-        {"Aguerre [GK]",    mfm::RoleId::GOALKEEPER,           {50, 55, 10, 10, 60, 10, 40, 72, 68, 70, 55, 60, 70, 10, 10, 84, 80, 75, 78}, 0},
-        {"Barboza [DF_R]",  mfm::RoleId::OFFENSIVE_FULLBACK, {70, 72, 75, 60, 55, 75, 68, 70, 68, 72, 82, 84, 65, 70, 65, 10, 10, 10, 10}, 1},
-        {"Gularte [DF_C]",  mfm::RoleId::CENTER_BACK,          {62, 65, 48, 45, 85, 45, 55, 78, 80, 75, 60, 75, 86, 82, 78, 10, 10, 10, 10}, 1},
-        {"Lemos [DF_C]",    mfm::RoleId::CENTER_BACK,          {65, 68, 50, 40, 80, 50, 60, 76, 78, 74, 65, 78, 82, 80, 75, 10, 10, 10, 10}, 1},
-        {"Olivera [DF_L]",  mfm::RoleId::DEFENSIVE_FULLBACK, {65, 68, 60, 45, 65, 68, 60, 74, 72, 70, 78, 80, 75, 78, 72, 10, 10, 10, 10}, 1},
-        {"Trindade [MC_D]", mfm::RoleId::DEFENSIVE_MIDFIELDER, {72, 70, 62, 50, 65, 55, 68, 78, 82, 76, 70, 85, 80, 84, 78, 10, 10, 10, 10}, 2},
-        {"R. Fernandez",    mfm::RoleId::BOX_TO_BOX,           {74, 75, 70, 65, 60, 68, 75, 78, 74, 75, 78, 86, 72, 76, 68, 10, 10, 10, 10}, 2},
-        {"Darias [MC_A]",   mfm::RoleId::ADVANCED_PLAYMAKER,   {80, 80, 78, 72, 55, 75, 82, 75, 65, 78, 72, 72, 65, 55, 50, 10, 10, 10, 10}, 2},
-        {"Umpierrez [EX_L]",mfm::RoleId::WINGER,               {72, 76, 80, 68, 55, 76, 70, 72, 60, 70, 85, 80, 65, 45, 40, 10, 10, 10, 10}, 3},
-        {"Batista [DC]",    mfm::RoleId::PRESSING_FORWARD,     {65, 72, 70, 80, 78, 55, 65, 80, 75, 78, 78, 82, 78, 50, 45, 10, 10, 10, 10}, 3},
-        {"Arezo [EX_R]",    mfm::RoleId::INSIDE_FORWARD,       {70, 75, 82, 75, 65, 72, 68, 75, 65, 74, 86, 78, 70, 45, 40, 10, 10, 10, 10}, 3}
-    };
-    return t;
-}
 
 void runRealTime() {
     std::filesystem::path logPath = std::filesystem::current_path() / "partido.txt";
     mfm::Logger::init(logPath.string());
     
-    auto home = armarNacional();
-    auto away = armarPenarol();
+    auto home = mfm::TeamBuilder::createNacional();
+    auto away = mfm::TeamBuilder::createPenarol();
     
-    mfm::SimulationLoop::run(home, away, 42, true); // true = verbose
+    mfm::SimulationLoop::run(home, away, 42, true);
     
     mfm::Logger::close();
 }
 
 void runBatchMode(int iterations) {
     std::cout << "\nSimulando " << iterations << " partidos (Modo Batch silencioso)...\n";
-    auto home = armarNacional();
-    auto away = armarPenarol();
+    auto home = mfm::TeamBuilder::createNacional();
+    auto away = mfm::TeamBuilder::createPenarol();
     
     int home_wins = 0, away_wins = 0, draws = 0;
     long total_goals = 0;
@@ -85,7 +29,7 @@ void runBatchMode(int iterations) {
     auto start = std::chrono::high_resolution_clock::now();
     
     for (int i = 0; i < iterations; ++i) {
-        mfm::Match m = mfm::SimulationLoop::run(home, away, 1337 + i, false); // false = calladito
+        mfm::Match m = mfm::SimulationLoop::run(home, away, 1337 + i, false);
         
         total_goals += (m.stats.goals_home + m.stats.goals_away);
         if (m.stats.goals_home > m.stats.goals_away) home_wins++;
@@ -104,7 +48,6 @@ void runBatchMode(int iterations) {
 }
 
 int main() {
-    // 1. Cargar pesos a memoria
     std::filesystem::path cfgPath = std::filesystem::current_path() / "weights.cfg";
     mfm::Config::getInstance().load(cfgPath.string());
 
